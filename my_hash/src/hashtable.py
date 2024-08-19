9897574156fcd8f4263973e888a548b059461d11
Jason Taylor <Jason.Taylor@vanderbilt.edu>
2024-08-19 13:46:53 -0500

    tests

---
+++ my_hash/src/hashtable.py
@@ -0,0 +1,5 @@
+# hashstable.py
+
+
+class HashTable:
+    pass
---
+++ my_hash/test/test_hashtable.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# test_hashtable.py
+
+from hastable import HashTable
+
+def test_should_create_hashtable():
+    assert HashTable() is not None
+
+
+
