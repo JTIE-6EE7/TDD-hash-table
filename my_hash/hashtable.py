d811213a46eb8786e9b40599fe1f973983fabe40
Jason Taylor <Jason.Taylor@vanderbilt.edu>
2024-08-19 14:05:19 -0500

    added test; passed

--- my_hash/hashtable.py
+++ my_hash/hashtable.py
@@ -2,9 +2,11 @@
 
 
 class HashTable:
-    def __init__(self, size):
-        pass
+    def __init__(self, capacity):
+        self.capacity = capacity
 
+    def __len__(self):
+        return self.capacity
 
 
 
--- my_hash/test_hashtable.py
+++ my_hash/test_hashtable.py
@@ -4,6 +4,7 @@
 from hashtable import HashTable
 
 def test_should_create_hashtable():
-    assert HashTable(size=100) is not None
+    assert HashTable(capacity=100) is not None
 
-
+def test_should_report_capacity():
+    assert len(HashTable(capacity=100)) == 100
